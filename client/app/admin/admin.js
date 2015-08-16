e13983d23d75a708c4c49e8342dfb203c6f4863a
Manoj Nama <manoj.nama@outlook.com>
2015-08-16 15:12:53 +0530

    Upgraded to angular-fullstack generator scaffolding

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('meanBackendApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
